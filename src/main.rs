cf478c389f33816afa5cbeb9b46813b8879763e8
Agustin Chiappe Berrini <jnieve@gmail.com>
2019-03-19 20:12:11 -0400

    Isolate processes
    Isolate process correctly from hostname
    
    Basically, we run the command passed by arguments in a new process that
    has a cloned `UTS` and doesn't share it with the box. We also make sure
    that we cleaned the environment.
    
    isolate the file system and the pid

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,74 @@
-use libc::{c_int, clone, wait, CLONE_NEWUTS, SIGCHLD};
-use std::env::{args, remove_var, vars};
-use std::ffi::c_void;
-use std::process::{Command, exit, id};
+#[macro_use]
+extern crate failure;
+
+use failure::Error;
+use libc::{c_char, c_int, chroot, chdir, clone, waitpid, CLONE_NEWPID, CLONE_NEWUTS, SIGCHLD};
+use std::env::{args, remove_var, set_var, vars};
+use std::ffi::{CString, c_void};
+use std::process::{Command, exit};
 
 const USAGE: &'static str = "USAGE: ruthless [image] [command]";
 const STACK_SIZE: usize = 65536;
+
+#[derive(Debug, Fail)]
+enum RunError {
+    #[fail(display = "Error creating execution command")]
+    ErrorExecutingCommand,
+}
+
+struct RunArgs {
+    args: Vec<String>,
+}
 
 fn clear_environment() {
     for (k, _) in vars() {
         remove_var(k);
     }
+    set_var("PATH", "/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin:/usr/local/sbin");
 }
 
-fn stack_memory() -> Vec<u8> {
-    Vec::with_capacity(STACK_SIZE)
+macro_rules! str_to_pointer {
+    ($str:expr) => {
+        CString::from_vec_unchecked($str.as_bytes().to_vec()).as_ptr()
+    }
 }
 
-fn run(program: String, args: Vec<String>) {
-    clear_environment();
-    Command::new(program)
-        .args(args)
-        .spawn()
-        .expect("Command failed to start");
-}
-
-fn jail(program: String, args: Vec<String>) -> c_int {
-    let parent = id();
-    let stack = stack_memory().as_mut_ptr() as *mut c_void;
+fn change_root(location: &str) {
     unsafe {
-        clone(move |_| {
-            run(program, args)
-        }, stack, CLONE_NEWUTS | SIGCHLD, 0)
+        chroot(str_to_pointer!(location));
+        chdir(str_to_pointer!("/"));
+    }
+}
+
+fn stack_memory() -> *mut c_void {
+    let mut s: Vec<c_void> = Vec::with_capacity(STACK_SIZE);
+    unsafe { s.as_mut_ptr().offset(STACK_SIZE as isize) }
+}
+
+extern "C" fn run(args: *mut c_void) -> c_int {
+    let run_args = unsafe { &mut *(args as *mut RunArgs) };
+    clear_environment();
+    change_root("/home/agustin/projects/ruthless/root");
+    Command::new(run_args.args[0].clone())
+        .args(run_args.args[1..].into_iter())
+        .spawn()
+        .expect("Command failed to start")
+        .wait()
+        .unwrap();
+    0
+}
+
+fn jail(args: Vec<String>) -> Result<c_int, Error> {
+    let stack = stack_memory();
+    let mut run_args = RunArgs { args };
+    let c_args: *mut c_void = &mut run_args as *mut _ as *mut c_void;
+    let id = unsafe {
+        clone(run, stack, CLONE_NEWPID | CLONE_NEWUTS | SIGCHLD, c_args)
+    };
+    if id < 0 {
+        Err(Error::from(RunError::ErrorExecutingCommand))
+    } else {
+        Ok(id)
     }
 }
 
@@ -45,8 +83,12 @@ fn main() {
     args.next();
     let image = args.next().unwrap();
 
-    let child_id = jail(args.next().unwrap(), args.collect());
-    unsafe {
-        wait(child_id);
+    let child_id = jail(args.collect()).unwrap();
+    let r = unsafe {
+        waitpid(child_id, std::ptr::null_mut(), 0)
+    };
+    if r < 0 {
+        eprintln!("Error on the execution of the command");
+        exit(1);
     }
 }
