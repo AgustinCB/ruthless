dfd124f39462d1023014084dd50c6cbdff481681
Agustin Chiappe Berrini <jnieve@gmail.com>
2019-03-27 06:42:16 -0400

    Create a function to get the image repository location
    It expects it to be a mounted btrfs filesystem

--- src/images.rs
+++ src/images.rs
@@ -1,15 +1,31 @@
 use crate::mount::MOUNTS_FILE;
 use failure::Error;
 use std::env::home_dir;
+use std::fs::read;
 use std::path::PathBuf;
 
 #[derive(Debug, Fail)]
 pub(crate) enum ImageError {
     #[fail(display="No home directory")]
     NoHomeDirectory,
+    #[fail(display="Invalid lib path encoding")]
+    InvalidLibPathEncoding,
+    #[fail(display="Lib path not mounted")]
+    LibPathNotMounted,
 }
+
+const LIB_LOCATION: &'static str = ".local/lib/ruthless";
 
 fn get_image_repository_path() -> Result<PathBuf, Error> {
     let home_path = home_dir().ok_or(ImageError::NoHomeDirectory)?;
-    Ok(home_path.join(".local/lib/ruthless"))
+    let lib_path = home_path.join(LIB_LOCATION);
+    let expected_content = format!(" {} btrfs ", lib_path.to_str().ok_or(ImageError::InvalidLibPathEncoding)?);
+    let mounts_bytes = read(MOUNTS_FILE)
+        .map_err(|e| Error::from(e))?;
+    let mounts_contents = String::from_utf8(mounts_bytes)?;
+    if mounts_contents.contains(&expected_content) {
+        Ok(lib_path)
+    } else {
+        Err(ImageError::LibPathNotMounted)?
+    }
 }
