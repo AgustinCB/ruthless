dce6f2fe8ce62e6f85ff3cf159655a01c583aef1
Agustin Chiappe Berrini <jnieve@gmail.com>
2019-04-18 06:31:33 -0400

    Parse magic number and version

--- src/btrfs_send.rs
+++ src/btrfs_send.rs
@@ -1,4 +1,8 @@
 use std::convert::TryFrom;
+use std::mem::transmute;
+
+const MAGIC_NUMBER: &'static [u8] =
+    &[0x62, 0x74, 0x72, 0x66, 0x73, 0x2d, 0x73, 0x74, 0x72, 0x65, 0x61, 0x6d, 0x00];
 
 enum BtrfsSendCommandType {
     SUBVOL,
@@ -81,6 +85,15 @@ enum BtrfsSendError {
 impl TryFrom<Vec<u8>> for BtrfsSend {
     type Error = BtrfsSendError;
     fn try_from(source: Vec<u8>) -> Result<BtrfsSend, Self::Error> {
+        let magic_number = &source[0..MAGIC_NUMBER.len()];
+        if magic_number != MAGIC_NUMBER {
+            return Err(BtrfsSendError::InvalidMagicNumber)
+        };
+        let mut version_numbers_to_convert = [0u8; 4];
+        version_numbers_to_convert.copy_from_slice(&source[MAGIC_NUMBER.len()..MAGIC_NUMBER.len()+4]);
+        let mut version: u32 = unsafe {
+            transmute::<[u8; 4], u32>(version_numbers_to_convert)
+        }.to_be();
         Err(BtrfsSendError::InvalidMagicNumber)
     }
 }
